e2f1ac3647b41593d9423716ea06df9f76e46f3a
megaton47 <seanonuoha@yahoo.com>
2021-07-24 02:59:03 +0100

    second commit empty API key

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,3 +1,3 @@
 class Strings{
-  static String news_url = 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=f30e310a24db4da287a712260b5517ac';
+  static String news_url = 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=YOUR-API-KEY';
 }
